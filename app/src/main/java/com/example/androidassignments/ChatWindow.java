b69d976711f65ec2d080779e5294cd24ba9b88a3
BobahhBoi <laux6790@mylaurier.ca>
2022-11-06 15:52:31 -0500

    Finished Assignment 3 part 1

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -2,8 +2,13 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +21,39 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "ChatWindow";
     public static Short message;
     Button send_button = null;
     EditText chat_text = null;
     ListView list_view = null;
     ChatAdapter messageAdapter = null;
     ArrayList<String> messages = new ArrayList<String>();
+    ChatDatabaseHelper db;
+    SQLiteDatabase sqldb;
+    static String TABLE_NAME = "Messages";
 
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        db = new ChatDatabaseHelper(this);
+        sqldb = db.getWritableDatabase();
+        Cursor cursor = sqldb.rawQuery("SELECT * from Messages", null);
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()){
+
+            Log.i(ACTIVITY_NAME, "SQL_MESSAGE:" + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            Log.i(ACTIVITY_NAME, "Cursor's column count=" + cursor.getColumnCount());
+            messages.add(cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            cursor.moveToNext();
+
+        }
+        for(int i = 0;i<cursor.getColumnCount();i++){
+            Log.i(ACTIVITY_NAME, "Column Name: " +  cursor.getColumnName(i));
+        }
 
         send_button = findViewById(R.id.sendButton);
         chat_text = findViewById(R.id.editText);
@@ -38,10 +65,14 @@ public class ChatWindow extends AppCompatActivity {
     }
     public void addMessage(View view) {
         String chat_message = chat_text.getText().toString();
+        ContentValues cvals = new ContentValues();
+        cvals.put(ChatDatabaseHelper.KEY_MESSAGE,chat_message);
+        sqldb.insert(ChatDatabaseHelper.TABLE_NAME, null, cvals);
         messages.add(chat_message);
         messageAdapter.notifyDataSetChanged();
         chat_text.setText("");
     }
+
     private class ChatAdapter extends ArrayAdapter<String> {
         public ChatAdapter(Context ctx) {
             super(ctx, 0);
@@ -69,5 +100,11 @@ public class ChatWindow extends AppCompatActivity {
             message.setText(getItem(position));
             return result;
         }
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        sqldb.close();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
     }
 }
